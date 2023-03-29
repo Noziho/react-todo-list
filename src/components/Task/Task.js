67b5d28ec787e8b273cc44a58a253c48040343ff
Noziho <noah.decroix3@gmail.com>
2023-03-29 13:12:32 +0200

    Checkbox

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,6 +4,7 @@ import {useState} from "react";
 export const Task = ({task, setTaskList, setIsTaskUpdated}) => {
     const [editTaskValue, setEditTaskValue] = useState('');
     const [isEditingTask, setIsEditingTask] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
 
     const handleDeleteTask = (taskId) => {
         setTaskList((prevList) => prevList.filter((task) => task.id !== taskId))
@@ -30,6 +31,14 @@ export const Task = ({task, setTaskList, setIsTaskUpdated}) => {
         handleEditTask(task.id);
     }
 
+    const handleCheck = (e) => {
+        setIsChecked(e.target.checked);
+    }
+
+    if (isChecked) {
+        console.log("Check")
+    }
+
     if (isEditingTask) {
         return (
             <input type="text" value={editTaskValue} placeholder={task.taskName} onChange={e => {setEditTaskValue(e.target.value)}} onBlur={handleEditOnBlur}/>
@@ -37,7 +46,8 @@ export const Task = ({task, setTaskList, setIsTaskUpdated}) => {
     } else {
         return (
             <div className='task'>
-                <p>{task.taskName}</p>
+                <input type="checkbox" name="isChecked" onClick={handleCheck}/>
+                <p className={isChecked ? "taskChecked" : "notCheck"}>{task.taskName}</p>
                 <button onClick={handleDelete}>Delete</button>
                 <button onClick={handleEditOnClick}>Edit</button>
             </div>
